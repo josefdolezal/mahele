f908b5d87490520b81a7f591540272d2abc6a282
Josef Dolezal <pepik.ml@gmail.com>
2018-05-11 23:46:42 +0200

    Create parser for enums

--- mahele-cli/src/SchemaParser.hs
+++ mahele-cli/src/SchemaParser.hs
@@ -1,6 +1,7 @@
 module SchemaParser where
 
 import Text.ParserCombinators.Parsec
+import qualified Data.Maybe
 
 data ModelType = Type
                | Enum
@@ -11,6 +12,17 @@ data Symbol = Assignment
             | Alternative
             | Option
             | Colon
+            | Apostrophe
+
+data ParsedEnum = ParsedEnum
+    { peIdentifier :: String
+    , peCases      :: [EnumCase]
+    } deriving (Show)
+        
+data EnumCase = EnumCase
+    { ecIdentifier :: String
+    , ecRawValue   :: Maybe String
+    } deriving (Show)
 
 instance Show ModelType where
     show Type = "type"
@@ -24,6 +36,45 @@ symbol Assignment  = '='
 symbol Alternative = '|'
 symbol Option      = '?'
 symbol Colon       = ':'
+symbol Apostrophe  = '\''
+
+enum :: GenParser Char st ParsedEnum
+enum = do
+    a  <- typeAnnotation Enum
+    cs <- many1 $ do { newline; enumCase }
+    return $ ParsedEnum a cs
+
+enumCase :: GenParser Char st EnumCase
+enumCase = do
+    spaces
+    alternative
+    spaces
+    c <- constructor
+    v <- optionalRawValue
+    return $ EnumCase c v
+
+typeAnnotation :: ModelType -> GenParser Char st String
+typeAnnotation modelType = do
+    string $ show modelType
+    spaces
+    name <- typename
+    spaces
+    assignment
+    return name
+
+constructor :: GenParser Char st String
+constructor = identifier
+
+optionalRawValue :: GenParser Char st (Maybe String)
+optionalRawValue = try (Just <$> rawValue)
+                   <|> do { spaces; return Nothing }
+
+rawValue :: GenParser Char st String
+rawValue = do
+    spaces
+    string $ show Cast
+    spaces
+    quoted identifier
 
 typename :: GenParser Char st String
 typename = identifier
@@ -39,6 +90,9 @@ identifier = do
     c <- letter
     cs <- many alphaNum
     return (c:cs)
+
+quoted :: GenParser Char st a -> GenParser Char st a
+quoted = between (char $ symbol Apostrophe) (char $ symbol Apostrophe)
 
 alternative :: GenParser Char st Char
 alternative = reservedSymbol Alternative
