05a042c3eb570faee21087f93acdadc67a5faf3f
Josef Dolezal <pepik.ml@gmail.com>
2018-06-16 22:54:23 +0200

    Create sample program

--- mahele-cli/app/Main.hs
+++ mahele-cli/app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Lib
+import qualified Language.Mahele.Parser as P
+import qualified Language.Swift.Pretty as P
+import           Turtle
+import qualified Data.Text as T
+
+inputFileArgument :: Parser T.Text
+inputFileArgument = argText "src" "Mahele source file"
 
 main :: IO ()
-main = print ""
+main = do
+    input <- options "Model files generator based on Mahele declaration" inputFileArgument
+    result <- P.parseMahele <$> readFile (T.unpack input)
+    case result of
+        Left e  -> print e
+        Right m -> putStrLn . unlines $ map P.prettySwift m
